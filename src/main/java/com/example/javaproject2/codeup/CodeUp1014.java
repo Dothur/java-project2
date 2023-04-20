7271acf9193b839370d37065b46bda1eda38e41e
dothur <craz1000@naver.com>
2023-04-20 15:31:51 +0900

    feat. CodeUp1014 문제풀이 - 두 개의 char 변수에 각각 입력값을 담아서 거꾸로 출력

---
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1014.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1014 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char cVal1 = sc.next().charAt(0);
+        char cVal2 = sc.next().charAt(0);
+        System.out.printf("%c %c", cVal2, cVal1);
+    }
+}
