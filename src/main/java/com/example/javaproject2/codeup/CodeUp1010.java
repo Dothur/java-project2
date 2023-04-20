54d5fb9de1b34ba64f8ce734d726db6fd9543a14
dothur <craz1000@naver.com>
2023-04-20 13:39:31 +0900

    fix. printf 를 사용하여 재풀이

--- src/main/java/com/example/javaproject2/codeup/CodeUp1010.java
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1010.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class CodeUp1010 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int number = sc.nextInt();
-        System.out.println(number);
+        System.out.printf("%d", sc.nextInt());
     }
 }
