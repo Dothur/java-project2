c103f3847e60f56250207384a7bd39c7aa192191
dothur <craz1000@naver.com>
2023-04-20 11:33:20 +0900

    feat. printf 사용하기 - 포맷팅 확인

---
+++ src/main/java/com/example/javaproject2/week1/day4/PrintfTwoValuesShortage.java
@@ -0,0 +1,7 @@
+package com.example.javaproject2.week1.day4;
+
+public class PrintfTwoValuesShortage {
+    public static void main(String[] args) {
+        System.out.printf("%s님 안녕하세요.\n%s", "김도훈", "안녕히가세요.");
+    }
+}
