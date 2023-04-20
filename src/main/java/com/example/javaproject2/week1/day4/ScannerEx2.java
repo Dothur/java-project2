f19f09e38c4409eba03c28bf14cdeafb23cd3c72
dothur <craz1000@naver.com>
2023-04-20 10:46:48 +0900

    feat. Scanner 두개의 숫자 입력받아 구분하여 출력

--- src/main/java/com/example/javaproject2/week1/day4/ScannerEx2.java
+++ src/main/java/com/example/javaproject2/week1/day4/ScannerEx2.java
@@ -3,8 +3,12 @@ package com.example.javaproject2.week1.day4;
 import java.util.Scanner;
 
 public class ScannerEx2 {
-    public void inputTwoNumbersAndPlus(){
+    public void inputTwoNumbersAndPlus() {
         Scanner sc = new Scanner(System.in);
-        System.out.println(sc.nextInt() + sc.nextInt());
+        System.out.println("두 개의 숫자를 엔터로 구분하여 입력하세요");
+        System.out.print("첫번째 숫자 : ");
+        System.out.println("첫번째 숫자는 " + sc.nextInt() + " 입니다.");
+        System.out.print("두번째 숫자 : ");
+        System.out.println("두번째 숫자는 " + sc.nextInt() + " 입니다.");
     }
 }
