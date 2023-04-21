62bc547fc9ac9e308f05bbe091a1f06dca0567bd
dothur <craz1000@naver.com>
2023-04-21 16:10:17 +0900

    feat: CodeUp1020 추가 - 주민번호 입력받아 형태 바꿔 출력하기 - Use Array => String[] , .next().split(":")

---
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1020.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1020 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] arr = sc.next().split("-");
+        System.out.printf("%s%s", arr[0], arr[1]);
+    }
+}
