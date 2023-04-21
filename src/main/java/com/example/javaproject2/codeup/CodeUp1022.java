1687c8ca33658e93621b2afd0f592af5d57806f3
dothur <craz1000@naver.com>
2023-04-21 16:12:27 +0900

    feat: CodeUp1022 추가 - 문장 1개 입력받아 그대로 출력하기 - Use .nextLine();

---
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1022.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1022 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine(); // nextLine() : 공백이 포함된 문장을 입력받음
+        System.out.println(str);
+    }
+}
