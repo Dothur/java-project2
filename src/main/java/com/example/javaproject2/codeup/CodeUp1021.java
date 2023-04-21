dd751a84960f950f937553e7b0d31ee5992828c1
dothur <craz1000@naver.com>
2023-04-21 16:11:34 +0900

    feat: CodeUp1021 추가 - 단어 1개 입력받아 그대로 출력하기

---
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1021.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1021 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.next();
+        System.out.printf("%s", word);
+    }
+}
