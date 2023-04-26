b568f28be50d91538173f5aa52f913bbd977ca3d
dothur <craz1000@naver.com>
2023-04-26 11:34:13 +0900

    feat. 2차원 10x10 배열 for 문 사용하기 실습

---
+++ src/main/java/com/example/javaproject2/week2/day3/TenXTenFor.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.week2.day3;
+
+import java.util.Arrays;
+
+public class TenXTenFor {
+    public static void main(String[] args) {
+        int[][] intArr = new int[10][10];
+        for(int i=0; i<10; i++){
+            System.out.println(Arrays.toString(intArr[i]));
+        }
+    }
+}
