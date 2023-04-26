1e066f4b1fd1cafd7569e1892de2b8a786d7ad4a
dothur <craz1000@naver.com>
2023-04-26 10:38:05 +0900

    feat. 2차원 배열 3x3, 10x10 실습 - Arrays.toString

---
+++ src/main/java/com/example/javaproject2/week2/day3/TenXTen.java
@@ -0,0 +1,19 @@
+package com.example.javaproject2.week2.day3;
+
+import java.util.Arrays;
+
+public class TenXTen {
+    public static void main(String[] args) {
+        int[][] intArr = new int[10][10]; // 배열 선언과 초기화
+        System.out.println(Arrays.toString(intArr[0]));
+        System.out.println(Arrays.toString(intArr[1]));
+        System.out.println(Arrays.toString(intArr[2]));
+        System.out.println(Arrays.toString(intArr[3]));
+        System.out.println(Arrays.toString(intArr[4]));
+        System.out.println(Arrays.toString(intArr[5]));
+        System.out.println(Arrays.toString(intArr[6]));
+        System.out.println(Arrays.toString(intArr[7]));
+        System.out.println(Arrays.toString(intArr[8]));
+        System.out.println(Arrays.toString(intArr[9]));
+    }
+}
---
+++ src/main/java/com/example/javaproject2/week2/day3/ThreeXThree.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.week2.day3;
+
+import java.util.Arrays;
+
+public class ThreeXThree {
+    public static void main(String[] args) {
+        int[][] intArr = new int[3][3];
+        System.out.println(Arrays.toString(intArr[0]));
+        System.out.println(Arrays.toString(intArr[1]));
+        System.out.println(Arrays.toString(intArr[2]));
+    }
+}
