a633af2e6ae444af6179b404d7eaa4f2322b03fe
dothur <craz1000@naver.com>
2023-04-27 15:27:23 +0900

    feat. CodeUp1071 ~ 1077 문제풀이 - 반복실행구조 - for , while

---
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1074.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1074 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int iVal = sc.nextInt();
+        while(iVal > 0){
+            System.out.println(iVal--);
+        }
+    }
+}
