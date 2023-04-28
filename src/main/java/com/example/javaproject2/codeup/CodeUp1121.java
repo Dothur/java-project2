8c8723f3622e5a80663d1bf6e090457de6cad680
dothur <craz1000@naver.com>
2023-04-28 15:46:03 +0900

    feat. CodeUp1119 ~ 1121 문제풀이 - 일을 시간으로 변환 - 세 수의 평균 - 나머지 구하기

---
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1121.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1121 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int aVal = sc.nextInt();
+        int bVal = sc.nextInt();
+        System.out.printf("%d", aVal % bVal);
+    }
+}
