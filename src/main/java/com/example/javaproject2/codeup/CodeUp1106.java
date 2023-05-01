b022afc5956c627b13fc235767b46bd6a6601649
dothur <craz1000@naver.com>
2023-05-01 13:09:09 +0900

    feat. CodeUp1103 , CodeUp1106 문제풀이 - 파일 위치출력 \" \" - MIN_VALUE MAX_VALUE

---
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1106.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.codeup;
+
+public class CodeUp1106 {
+    public static void main(String[] args) {
+        int lowVal = Integer.MIN_VALUE;
+        int highVal = Integer.MAX_VALUE;
+        System.out.printf("%d %d", lowVal, highVal);
+    }
+}
