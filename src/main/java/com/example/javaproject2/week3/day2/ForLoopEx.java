170fe343f634435a43625fbfadb8ccfd78df6d71
dothur <craz1000@naver.com>
2023-05-02 11:04:36 +0900

    feat. for 문 실습 - 조건식 i*i 로 루트값 까지 출력하기 - 조건식에 2*i 로 2부터 9까지만 출력하기

---
+++ src/main/java/com/example/javaproject2/week3/day2/ForLoopEx.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.week3.day2;
+
+public class ForLoopEx {
+    public static void main(String[] args) {
+        for (int i = 2; 2 * i <= 18; i++) {
+            System.out.println(i);
+        }
+    }
+}
