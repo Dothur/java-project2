0912cd24cf5ab91b9492fbb7fe22c520f01c0ac9
dothur <craz1000@naver.com>
2023-05-04 15:24:05 +0900

    fix. CodeUp1099 수정 - 개미의 이동경로 while 문 내에 if else 문 간결하게 수정

--- src/main/java/com/example/javaproject2/codeup/CodeUp1099.java
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1099.java
@@ -13,21 +13,16 @@ public class CodeUp1099 {
             }
         }
         // 개미의 이동경로
-        int x = 1;
-        int y = 1;
+        int x = 1, y = 1;
         while (true) {
             if (inputBox[x][y] == 2) {
                 inputBox[x][y] = 9;
                 break;
             }
             inputBox[x][y] = 9;
-            if (inputBox[x][y + 1] != 1) {
-                y++;
-            } else if (inputBox[x + 1][y] != 1) {
-                x++;
-            } else {
-                break;
-            }
+            if (inputBox[x][y + 1] != 1) y++;
+            else if (inputBox[x + 1][y] != 1) x++;
+            else break;
         }
         // 상자 배열 출력
         for (int i = 0; i < 10; i++) {
