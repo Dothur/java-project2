012c69d7516f81052a175387142d62373f7c9ec8
dothur <craz1000@naver.com>
2023-05-08 09:39:29 +0900

    feat. 직각 삼각형 별찍기 실습

---
+++ src/main/java/com/example/javaproject2/week4/day1/starEx.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.week4.day1;
+
+public class starEx {
+    public static void main(String[] args) {
+        int num = 5;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
