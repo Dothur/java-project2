df42c785db2d7e9d9cbe806340ac40bd228911a0
dothur <craz1000@naver.com>
2023-05-09 16:43:30 +0900

    fix. 주석 추가

--- src/main/java/com/example/javaproject2/week4/day2/DiEx.java
+++ src/main/java/com/example/javaproject2/week4/day2/DiEx.java
@@ -1,8 +1,8 @@
 package com.example.javaproject2.week4.day2;
 
 public class DiEx {
-    private ShapeDrawer shapeDrawer;
-    public DiEx(ShapeDrawer shapeDrawer){
+    private ShapeDrawer shapeDrawer; // 참조변수 ( ShapeDrawer )
+    public DiEx(ShapeDrawer shapeDrawer){ // 생성자 (param = 객체)
         this.shapeDrawer = shapeDrawer;
     }
     public void doSth(){
