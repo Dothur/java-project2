1f57d5d03151b72789e276a4c83c16ab96d6e06d
dothur <craz1000@naver.com>
2023-05-09 11:33:07 +0900

    fix. rename, getheight() 호출

--- src/main/java/com/example/javaproject2/week4/day2/PyramidClass.java
+++ src/main/java/com/example/javaproject2/week4/day2/PyramidClass.java
@@ -1,37 +1,44 @@
 package com.example.javaproject2.week4.day2;
 
-public class ReversePyramid {
+public class PyramidClass {
     private final int height;
     private final String spaceChar;
     private final String star;
-    private ReversePyramid(int height, String spaceChar, String star) {
+
+    private PyramidClass(int height, String spaceChar, String star) {
         this.spaceChar = spaceChar;
         this.height = height;
         this.star = star;
     }
-    private void printPyramid(){
+
+    private void printPyramid() {
         for (int i = 0; i < height; i++) {
             System.out.print(makePyramidLine(height, i));
         }
     }
-    private void printParallelogram(){
+
+    private void printParallelogram() {
         for (int i = 0; i < getHeight(); i++) {
             System.out.print(makeParallelogramALine(getHeight(), i));
         }
     }
-    private int getHeight(){
+
+    public int getHeight() {
         return height;
     }
 
-    private String makePyramidLine(int height, int i){
-       return String.format("%s%s\n", spaceChar.repeat(i), star.repeat(height * 2 - 1 - i * 2));
+    private String makePyramidLine(int height, int i) {
+        return String.format("%s%s\n", spaceChar.repeat(i), star.repeat(height * 2 - 1 - i * 2));
     }
+
     private String makeParallelogramALine(int height, int i) {
         return String.format("%s%s\n", spaceChar.repeat(i), star.repeat(height));
     }
+
     public static void main(String[] args) {
-        ReversePyramid reversePyramid = new ReversePyramid(5, " ", "#");
-        reversePyramid.printPyramid();
-        reversePyramid.printParallelogram();
+        PyramidClass pyramidclass = new PyramidClass(5, " ", "#");
+        pyramidclass.printPyramid();
+        pyramidclass.printParallelogram();
+        System.out.println(pyramidclass.getHeight());
     }
 }
