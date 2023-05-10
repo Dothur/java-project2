c4ef94abc8f7e13eaf61afa0aaca33259da86578
dothur <craz1000@naver.com>
2023-05-10 16:17:46 +0900

    fix. 주석 내용 수정

--- src/main/java/com/example/javaproject2/week4/day2/ShapeDrawer.java
+++ src/main/java/com/example/javaproject2/week4/day2/ShapeDrawer.java
@@ -7,5 +7,5 @@ public abstract class ShapeDrawer {
             System.out.print(makeALine(height, i)); // 추상 메소드를 printShape method 로 출력
         }
     }
-    public abstract String makeALine(int h, int i); // abstract 로 pyramid2 의 makeALine 추상화
+    public abstract String makeALine(int h, int i); // abstract 로 makeALine 추상 메소드 정의
 }
